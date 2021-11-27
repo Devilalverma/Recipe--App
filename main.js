895580095b11c32e0e1dff5b1113204b24cc6e55
Diavyansh <86036331+Devilalverma@users.noreply.github.com>
2021-11-27 18:11:46 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function pizza(){
+    document.getElementById("output").src="pizza.jpg";
+    
+    
+}
+function cake(){
+    document.getElementById("output").src="https://i.pinimg.com/originals/7c/c9/c3/7cc9c3ba912e2432a58cf114792c3739.jpg";
+    
+}
+function pasta(){
+    document.getElementById("output").src="Untitled.png";
+}
+
